2c4a55b8460cd2ea4d1c8b54ba694aaaec9a0ea2
Ishaan <agrawalishaan115@gmail.com>
2021-08-21 22:19:37 +0530

    Finished making quizzler 2.0

--- quizzler_app/quiz_brain.py
+++ quizzler_app/quiz_brain.py
@@ -15,6 +15,7 @@ class QuizBrain:
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
+        print(self.question_number)
         q_text = html.unescape(self.current_question.text)
         return f"Q.{self.question_number}: {q_text}"
         # user_answer = input(f"Q.{self.question_number}: {q_text} (True/False): ")
@@ -24,9 +25,6 @@ class QuizBrain:
         correct_answer = self.current_question.answer
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
-            print("You got it right!")
+            return True
         else:
-            print("That's wrong.")
-
-        print(f"Your current score is: {self.score}/{self.question_number}")
-        print("\n")
+            return False
--- quizzler_app/ui.py
+++ quizzler_app/ui.py
@@ -6,7 +6,9 @@ THEME_COLOR = "#375362"
 
 class QuizInterface:
     def __init__(self, quiz_brain: QuizBrain):
+        self.reps = 0
         self.quiz = quiz_brain
+        self.user_answer = None
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
@@ -25,11 +27,21 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         correct = PhotoImage(file="images/true.png")
-        self.correct_button = Button(image=correct, highlightthickness=0, border=1, bg=THEME_COLOR)
+        self.correct_button = Button(
+            image=correct,
+            highlightthickness=0,
+            border=1, bg=THEME_COLOR,
+            command=self.user_answer_true
+        )
         self.correct_button.grid(column=0, row=2)
 
         wrong = PhotoImage(file="images/false.png")
-        self.wrong_button = Button(image=wrong, highlightthickness=0, border=1, bg=THEME_COLOR)
+        self.wrong_button = Button(
+            image=wrong,
+            highlightthickness=0,
+            border=1, bg=THEME_COLOR,
+            command=self.user_answer_false
+        )
         self.wrong_button.grid(column=1, row=2)
 
         self.get_next_question()
@@ -37,5 +49,29 @@ class QuizInterface:
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question, text=q_text)
+        self.canvas.config(bg="white")
+        self.score.config(text=f"Score: {self.quiz.score}")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question, text=f"You have reached the end of the quiz. Your score is {self.quiz.score}/{self.quiz.question_number}")
+            self.correct_button.config(state="disabled")
+            self.wrong_button.config(state="disabled")
+
+    def user_answer_true(self):
+        self.user_answer = "True"
+        is_right = self.quiz.check_answer(self.user_answer)
+        self.give_feedback(is_right)
+
+    def user_answer_false(self):
+        self.user_answer = "False"
+        is_right = self.quiz.check_answer(self.user_answer)
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_correct):
+        if is_correct:
+            self.canvas.config(bg="green")
+        elif not is_correct:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
